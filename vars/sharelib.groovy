79851c095d7e175cfb41324c73f5da3b2bb9430d
Devops-aws848 <rajesh.merugu848@gmail.com>
2025-12-14 23:31:10 +0530

    Update sharelib.groovy

--- vars/sharelib.groovy
+++ vars/sharelib.groovy
@@ -1,1 +1,24 @@
-
+stage('compiler')
+    {
+        sh "${mvn}/bin/mvn clean compile"
+    }
+    stage('build')
+    {
+        sh "${mvn}/bin/mvn clean package"
+    }
+    stage('quality')
+    {
+        notifyBuild('STARTED')
+        sh "${mvn}/bin/mvn sonar:sonar"
+    }
+    
+        stage('deploy')
+    {
+      sh    """
+        
+        curl -u admin:password \
+        --upload-file /var/lib/jenkins/workspace/Bsnl/target/maven-web-application.war \
+        "http://13.234.67.81:8080/manager/text/deploy?path=/maven-web-application&update=trye"
+        """
+     }
+    
